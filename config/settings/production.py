ebe982df4e47281a67809355c0e6f14602f45a1e
iv-thinkpad <gewooniv@gmail.com>
2024-08-01 09:58:16 +0200

    production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -68,7 +68,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ if os.getenv("READTHEDOCS", default=False) == "True":
 else:
     sys.path.insert(0, os.path.abspath("/app"))
 os.environ["DATABASE_URL"] = "sqlite:///readthedocs.db"
-os.environ["CELERY_BROKER_URL"] = os.getenv("REDIS_URL", "redis://redis:6379")
+os.environ["CELERY_BROKER_URL"] = os.getenv("REDIS_URL")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
